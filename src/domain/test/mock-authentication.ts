fb0c8815967e1000e346730dbba2634a7673fe8b
“tiagoluis12” <tiago7164@hotmail.com>
2024-01-28 12:05:10 +0000

    refactor: change imports to use module

--- src/domain/test/mock-authentication.ts
+++ src/domain/test/mock-authentication.ts
@@ -1,4 +1,4 @@
-import { AutheticationParams } from "../usecases/authentication";
+import { AutheticationParams } from "@/domain/usecases/authentication";
 import { faker } from '@faker-js/faker'
 
 export const mockAuthentication = (): AutheticationParams => ({
